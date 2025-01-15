498bfd51c56a62c59784d651efdc0230dae8b272
Shravani Rajguru <sraj0068@student.monash.edu>
2025-01-15 19:45:21 +1100

    Calculating powerDiff and baseDiff for colliders

--- public/bn.route.js
+++ public/bn.route.js
@@ -965,7 +965,6 @@ module.exports = {
 			let origNet = null;
 			try {
 				console.log('prepareData');
-
 				/// tbd
 				let bnKey;
 				let bn =  {};
@@ -1174,10 +1173,20 @@ module.exports = {
 						const baselineProb = baselineBelief[targetStateIndex];
 						console.log('baselineProb:', baselineProb)
 
-						let sentences = [];
-						let totalInfluencePercentage = 0; 
+						let pathWithRelationship = []
 
-						for (let nonActiveNodeName of Object.keys(evidence)) {
+						const collider = analyzeColliders(
+							net,
+							relationships,
+							evidence,
+							targetNodeName,
+							targetStateIndex,
+							bnKey
+						);
+						
+						console.log("Detected Colliders and Differences:", collider);
+
+						for (let evidenceNodeName of Object.keys(evidence)) {
 							// Initialize a temporary array to store the sentences generated for this specific nonActiveNode.
 							// let nodeSentences = [];
 						
@@ -1210,8 +1219,9 @@ module.exports = {
 											
 							// activePaths = filterShortestPaths(ActivePaths);							
 						
-							const nonActiveNodes = Object.keys(evidence);
-							console.log("ActivePaths: ", activePaths);
+							// const nonActiveNodes = Object.keys(evidence);
+							console.log("activePaths: ", activePaths);
+
 						
 							// For each filtered path, generate a sentence describing how the current nonActiveNode influences the target.
 							for (const path of activePaths) {
